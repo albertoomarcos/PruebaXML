0b43fa1b7f2b0c331ef8394706acc6ffb57c10d7
Alberto Marcos Blasco <38374836+albertoomarcos@users.noreply.github.com>
2019-03-01 12:38:42 +0100

    Translate finalizado
    Falta realizar pruebas

--- WebContent/pruebaxml/vistaPrincipal.controller.js
+++ WebContent/pruebaxml/vistaPrincipal.controller.js
@@ -53,8 +53,9 @@ sap.ui.controller("pruebaxml.vistaPrincipal", {
 sap.ui.define([
    "sap/ui/core/mvc/Controller",
    "sap/m/MessageToast",
-   "sap/ui/model/json/JSONModel"
-], function (Controller, MessageToast, JSONModel) {
+   "sap/ui/model/json/JSONModel",
+   "sap/ui/model/resource/ResourceModel"
+], function (Controller, MessageToast, JSONModel, ResourceModel) {
    "use strict";
    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
       onInit : function () {
@@ -66,9 +67,22 @@ sap.ui.define([
          };
          var oModel = new JSONModel(oData);
          this.getView().setModel(oModel);
+         
+         // set i18n_es model en la vista
+         var i18nModel = new ResourceModel({
+        //     bundleName: "sap.ui.demo.walkthrough.i18n.i18n"
+        	 bundleName: "i18n.i18n"
+          });
+         this.getView().setModel(i18nModel, "i18n");
+         
       },
       onShowHello : function () {
-         MessageToast.show("Hello World");
+    	  // leemos el mensaje de i18n_es model
+          var oBundle = this.getView().getModel("i18n").getResourceBundle();
+          var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+          var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+          // show message
+          MessageToast.show(sMsg);
       }
    });
 });
