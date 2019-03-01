7860f65bf5be58045eacf90fe3282cb06aefc8a0
Alberto Marcos Blasco <38374836+albertoomarcos@users.noreply.github.com>
2019-03-01 11:53:11 +0100

    Cambio en el controlador de Ventana Principal
    Se arregla el TOAST

--- WebContent/pruebaxml/vistaPrincipal.controller.js
+++ WebContent/pruebaxml/vistaPrincipal.controller.js
@@ -1,10 +1,11 @@
+/*
 sap.ui.controller("pruebaxml.vistaPrincipal", {
 
-/**
-* Called when a controller is instantiated and its View controls (if available) are already created.
-* Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-* @memberOf pruebaxml.vistaPrincipal
-*/
+
+ Called when a controller is instantiated and its View controls (if available) are already created.
+ Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+ @memberOf pruebaxml.vistaPrincipal
+
 	onInit: function() {
 		var oData = {
 	            recipient : {
@@ -22,30 +23,53 @@ sap.ui.controller("pruebaxml.vistaPrincipal", {
 	{
 		 alert(a);
 	}
-/**
-* Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-* (NOT before the first rendering! onInit() is used for that one!).
-* @memberOf pruebaxml.vistaPrincipal
-*/
-//	onBeforeRendering: function() {
-//
-//	},
+ Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+ (NOT before the first rendering! onInit() is used for that one!).
+ @memberOf pruebaxml.vistaPrincipal
 
-/**
-* Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-* This hook is the same one that SAPUI5 controls get after being rendered.
-* @memberOf pruebaxml.vistaPrincipal
-*/
-//	onAfterRendering: function() {
-//
-//	},
+	onBeforeRendering: function() {
 
-/**
-* Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-* @memberOf pruebaxml.vistaPrincipal
-*/
-//	onExit: function() {
-//
-//	}
+	},
+
+
+ Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+ This hook is the same one that SAPUI5 controls get after being rendered.
+ @memberOf pruebaxml.vistaPrincipal
+
+	onAfterRendering: function() {
+
+	},
+
+
+ Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+ @memberOf pruebaxml.vistaPrincipal
+
+	onExit: function() {
+
+	}
 
 });
+*/
+sap.ui.define([
+   "sap/ui/core/mvc/Controller",
+   "sap/m/MessageToast",
+   "sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
+   "use strict";
+   return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+      onInit : function () {
+         // set data model on view
+         var oData = {
+            recipient : {
+               name : "World"
+            }
+         };
+         var oModel = new JSONModel(oData);
+         this.getView().setModel(oModel);
+      },
+      onShowHello : function () {
+         MessageToast.show("Hello World");
+      }
+   });
+});
+
